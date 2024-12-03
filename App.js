545182b258536c5167a92ede6fc2f7396348f1de
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2024-12-03 11:35:37 -0300

    estrutura base criada, agora é cada um dar um pull na sua branch

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
+import 'react-native-gesture-handler';
+import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
+import { Loading } from './src/components/Loading/Index';
 
 export default function App() {
+
+  const [fontesLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold
+  })
+
+  if (!fontesLoaded) {
+    return <Loading />
+  }
+
   return (
     <View >
       <Text>Projeto de bloco RN</Text>
---
+++ src/routes/index.jsx
@@ -0,0 +1,26 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+//import das screens
+
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+
+    </Stack.Navigator>
+  )
+}
+
+
+export default function Routes() {
+  <NavigationContainer>
+
+  </NavigationContainer>
+}
